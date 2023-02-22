98c7823cc232e6f1de29e7b222e11441b556b6f7
Alysson-Macharete <alysson.macharete1@gmail.com>
2023-02-22 18:07:20 -0300

    primeiro commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Criando as imagens..."
+
+docker build -t alyssonmacharete/projeto-backend:1.0 backend/.
+docker build -t alyssonmacharete/projeto-database:1.0 database/.
+
+echo "Realizando push das imagens..."
+
+docker push alyssonmacharete/projeto-backend:1.0
+docker push alyssonmacharete/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes ... "
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments ... "
+kubectl apply -f ./deployment.yml
